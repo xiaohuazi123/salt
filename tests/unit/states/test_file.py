cb860a7c1650340ed09d47b7b4eb3690be51f0a5
Pedro Algarvio <pedro@algarvio.me>
2020-04-17 18:00:37 +0100

    Replace `@destructiveTest` with `@pytest.mark.destructive_test`

--- tests/unit/states/test_file.py
+++ tests/unit/states/test_file.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Import python libs
 from __future__ import absolute_import, print_function, unicode_literals
 
 import os
@@ -8,13 +7,12 @@ import pprint
 import shutil
 from datetime import datetime
 
+import pytest
 import salt.modules.file as filemod
 import salt.serializers.json as jsonserializer
 import salt.serializers.python as pythonserializer
 import salt.serializers.yaml as yamlserializer
 import salt.states.file as filestate
-
-# Import salt libs
 import salt.utils.files
 import salt.utils.json
 import salt.utils.platform
@@ -22,9 +20,6 @@ import salt.utils.win_functions
 import salt.utils.yaml
 from salt.exceptions import CommandExecutionError
 from salt.ext.six.moves import range
-from tests.support.helpers import destructiveTest
-
-# Import Salt Testing libs
 from tests.support.mixins import LoaderModuleMockMixin
 from tests.support.mock import MagicMock, Mock, call, mock_open, patch
 from tests.support.runtests import RUNTIME_VARS
@@ -2763,7 +2758,7 @@ class TestFilePrivateFunctions(TestCase, LoaderModuleMockMixin):
     def setup_loader_modules(self):
         return {filestate: {"__salt__": {"file.stats": filemod.stats}}}
 
-    @destructiveTest
+    @pytest.mark.destructive_test
     @skipIf(salt.utils.platform.is_windows(), "File modes do not exist on windows")
     def test__check_directory(self):
         """
